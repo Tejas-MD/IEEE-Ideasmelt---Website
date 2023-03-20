b6f23c04a0ae3bd869fba12eae9bb28a513454b4
Tejas MD <tejasmutalikdesai@gmail.com>
2023-03-20 23:34:10 +0530

    💄 Added hero banner and upgraded UI
    💄Centered the submit solution button

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-document.getElementById("button").addEventListener("click", function() {
-    window.open("https://www.google.com/forms/", "_blank");
-  });
-  
+  const btn = document.querySelector("#btn");
+  const btnText = document.querySelector("#btnText");
+
+  btn.onclick = () => {
+      btnText.innerHTML = "Thanks";
+      btn.classList.add("active");
+      window.open("https://www.google.com/forms/", "_blank");
+
+  };
